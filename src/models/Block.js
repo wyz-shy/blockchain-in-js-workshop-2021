ee028364bb9857c6d45296e5b48669ae959b57cf
wyz-shy <1912759877@qq.com>
2023-05-18 16:54:22 +0800

    1

--- src/models/Block.js
+++ src/models/Block.js
@@ -1,9 +1,18 @@
+import sha256 from 'crypto-js/sha256.js'
 class Block {
   // 1. 完成构造函数及其参数
   /* 构造函数需要包含
 
   */
-  constructor() {}
-}
+    constructor(blockchain, previousHash, height, hash) {
+      this.blockchain = blockchain
+      this.previousHash = previousHash
+      this.height = height
+      this.hash = hash
+    }
+    
+  }
+
+  
 
 export default Block
--- src/models/Blockchain.js
+++ src/models/Blockchain.js
@@ -1,3 +1,5 @@
+import { unfold, values } from "ramda";
+import Block from "./Block.js";
 // Blockchain
 class Blockchain {
   // 1. 完成构造函数及其参数
@@ -6,17 +8,45 @@ class Blockchain {
       - 创世区块
       - 存储区块的映射
   */
-  constructor() {}
+ 
+      constructor(name) {
+        this.name = name
+        this.blocks = {}
+        this.difficulty = 2
+        this.genesis = null
+      }
+    
+    
 
+    
+    
+    
+    
   // 2. 定义 longestChain 函数
   /* 
     返回当前链中最长的区块信息列表
   */
-  longestChain() {
-
-
-    return []
-  }
+    longestChain() {
+      let longestChain = [this.genesis]
+  
+      for (let hash in this.blocks) {
+        let block = this.blocks[hash]
+        let chain = [block]
+  
+        while (chain[0].previousHash !== this.genesis.hash) {
+          chain.unshift(this.blocks[chain[0].previousHash])
+        }
+  
+        if (chain.length > longestChain.length) {
+          longestChain = chain
+        }
+      }
+  
+      return longestChain
+    }
 }
+  
+       
+  
 
 export default Blockchain
