25c5dc7c0427cc149c0693a61320fa05e1010b8e
wyz-shy <1912759877@qq.com>
2023-06-02 09:09:52 +0800

    1

--- src/models/Merkletree.js
+++ src/models/Merkletree.js
@@ -1,149 +1,102 @@
-const CryptoJS = require('crypto-js');
-const hashFunction = (value) => {
-    return CryptoJS.SHA256(value.toString()).toString();
-};
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
 
-class Node {
-    constructor(value) {
-        this.value = value;
-        this.hash = hashFunction(value);
-    }
-}
+
+
+const CryptoJS = require('crypto-js');
+
 
 class MerkleTree {
-    constructor(data) {
-        this.leaves = data.map((value) => new Node(value));
-        this.root = this.buildTree(this.leaves);
-    }
-
-    buildTree(nodes) {
-        if (nodes.length === 1) {
-            return nodes[0];
-        }
-        const parents = [];
-        if (nodes.length % 2 !== 0) {
-            nodes.push(nodes[nodes.length - 1]);
-        }
-        for (let i = 0; i < nodes.length; i += 2) {
-            const left = nodes[i];
-            const right = nodes[i + 1];
-            const parent = new Node(left.hash + right.hash);
-            parent.left = left;
-            parent.right = right;
-            left.parent = parent;
-            right.parent = parent;
-            parents.push(parent);
-        }
-        const root = this.buildTree(parents);
-        return root;
-    }
-
-    addNode(value) {
-        const node = new Node(value);
-        const lastNode = this.leaves[this.leaves.length - 1];
-        const parent = new Node(lastNode.hash + node.hash);
-        parent.left = lastNode;
-        parent.right = node;
-        lastNode.parent = parent;
-        node.parent = parent;
-        const rootHeight = this.getHeight(this.root);
-        const parentHeight = this.getHeight(parent);
-        if (parentHeight <= rootHeight) {
-            this.leaves.splice(this.leaves.length - 1, 1, node);
-            this.root = this.buildTree(this.leaves);
-        } else {
-            this.leaves.push(parent);
-            this.root = this.buildTree(this.leaves);
-        }
-    }
-
-    deleteNode(value) {
-      const node = this.leaves.find((leaf) => leaf.value === value);
-      if (!node) {
-          return false;
-      }
-  
-      const parent = node.parent;
-      if (!parent) {
-          // node is root
-          this.leaves = [];
-          this.root = null;
-          return;
-      }
-  
-      const nodeIndex = this.leaves.indexOf(node);
-      const sibling = parent.left === node ? parent.right : parent.left;
-      if (nodeIndex < this.leaves.indexOf(sibling)) {
-          this.leaves.splice(nodeIndex, 1);
-          this.leaves.splice(this.leaves.indexOf(sibling), 1);
-      } else {
-          this.leaves.splice(this.leaves.indexOf(sibling), 1);
-          this.leaves.splice(nodeIndex, 1);
-      }
-      if (this.leaves.length === 1) {
-          this.root = this.leaves[0];
-      } else {
-          this.root = this.buildTree(this.leaves);
-      }
-      this.updateParents(parent);
-      return true;
+  constructor(elements) {
+    this.elements = elements;
+    this.levels = this.buildLevels(elements);
+    this.root = this.levels[this.levels.length - 1][0];
   }
 
-    updateParents(parent) {
-        if (!parent) {
-            return;
-        }
-        const left = parent.left;
-        const right = parent.right;
-        parent.hash = hashFunction(left.hash + right.hash);
-        this.updateParents(parent.parent);
+  buildLevels(elements) {
+    if (elements.length === 0) {
+      throw new Error('No elements provided.');
     }
 
-    verify(value) {
-      const node = this.findNode(value);
-      if (!node) {
-          return false;
+    const levels = [];
+    levels.push(elements.map((el) => this.hash(el)));
+
+    while (levels[levels.length - 1].length > 1) {
+      const prevLevel = levels[levels.length - 1];
+      const currLevel = [];
+
+      for (let i = 0; i < prevLevel.length; i += 2) {
+        const left = prevLevel[i];
+        const right = i + 1 < prevLevel.length ? prevLevel[i + 1] : left;
+        const hash = this.combineHashes(left, right);
+        currLevel.push(hash);
       }
-  
-      let current = node;
-      let sibling;
-      let hash = node.hash;
-  
-      while (true) {
-          const parent = current.parent;
-          if (!parent) {
-              break;
-          }
-          if (parent.left === current) {
-              sibling = parent.right;
-          } else {
-              sibling = parent.left;
-          }
-          hash = hashFunction(sibling.hash + hash);
-          current = parent;
-      }
-      return hash === this.root.hash;
+
+      levels.push(currLevel);
+    }
+
+    return levels;
   }
 
-    findNode(value) {
-        return this.leaves.find((leaf) => leaf.value === value);
+  hash(data) {
+    return CryptoJS.SHA256(data.toString()).toString();
+  }
+
+  combineHashes(left, right) {
+    return this.hash(left + right);
+  }
+
+  getElementProof(element) {
+    const index = this.elements.indexOf(element);
+    if (index === -1) {
+      throw new Error('Element not found in the Merkle tree.');
     }
 
-    getHeight(node) {
-        if (!node) {
-            return 0;
-        }
-        const leftHeight = this.getHeight(node.left);
-        const rightHeight = this.getHeight(node.right);
-        return Math.max(leftHeight, rightHeight) + 1;
-    };
+    const proof = [];
+    let levelIndex = 0;
+    let currentIndex = index;
+
+    while (levelIndex < this.levels.length - 1) {
+      const level = this.levels[levelIndex];
+      const isLeftNode = currentIndex % 2 === 0;
+      const siblingIndex = isLeftNode ? currentIndex + 1 : currentIndex - 1;
+
+      if (siblingIndex < level.length) {
+        proof.push({
+          position: isLeftNode ? 'right' : 'left',
+          data: level[siblingIndex],
+        });
+      }
+
+      currentIndex = Math.floor(currentIndex / 2);
+      levelIndex++;
+    }
+
+    return proof;
+  }
+
+  verifyElementProof(element, proof) {
+    const elementHash = this.hash(element);
+    let hash = elementHash;
+
+    for (const proofElement of proof) {
+      const siblingHash = proofElement.data;
+      const isLeftNode = proofElement.position === 'left';
+      hash = isLeftNode ? this.combineHashes(siblingHash, hash) : this.combineHashes(hash, siblingHash);
+    }
+
+    return hash === this.root;
+  }
 }
 
-const mt = new MerkleTree(['b', 'c','a']);
-console.log(mt.verify('a')); // true
-mt.addNode('d');
-console.log(mt.verify('d')); // true
-console.log(mt.verify('f')); // false
-mt.deleteNode('c');
-console.log(mt.verify('c')); // false
-console.log(mt.getHeight(mt.root)); // 2
+// 示例用法：
+const elements = ['element1', 'element2', 'element3', 'element4'];
+const merkleTree = new MerkleTree(elements);
+console.log('Merkle Root:', merkleTree.root);
+
+const elementToVerify = 'element3';
+const proof = merkleTree.getElementProof(elementToVerify);
+console.log('Proof:', proof);
+
+const isVerified = merkleTree.verifyElementProof(elementToVerify, proof);
+console.log('Is Verified:', isVerified);
